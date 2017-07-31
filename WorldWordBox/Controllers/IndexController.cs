28fb19632ee5908e55f39cae7755a5ab82491584
hüsrev beyazışık <husrev.beyazisik@gmail.com>
2017-07-31 20:23:07 +0300

    Commit Problem

---
+++ WorldWordBox/Controllers/IndexController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using WorldWordBox.Models;
+using CryptoHelper;
+
+namespace WorldWordBox.Controllers
+{
+
+
+    public enum RegisterStatus
+    {
+        Success,
+        Fail,
+        MailExist
+    }
+
+    public enum LoginStatus
+    {
+        Success,
+        UserNotExist,
+        IncorrectPassword,
+        Fail,
+    }
+
+    public class IndexController : Controller
+    {
+
+
+        wwbEntities entities;
+
+
+        public bool isLogged()
+        {
+            if (Session[Sys.userId] != null)
+                return true;
+
+            return false;
+        }
+
+
+        // GET: Index
+        public ActionResult Index()
+        {
+            if(isLogged())
+                return RedirectToAction("Index", "Member");
+
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult Login(string mail,string password,bool rememberMe)
+        {
+            try
+            {
+                using (entities = new wwbEntities())
+                { 
+                    Users userControl = entities.Users
+                               .Where(u => u.mail == mail)
+                               .FirstOrDefault();
+                
+
+                    if (userControl == null)
+                        return Json(LoginStatus.UserNotExist, JsonRequestBehavior.DenyGet);
+
+                    if (!Crypto.VerifyHashedPassword(userControl.password, password))
+                        return Json(LoginStatus.IncorrectPassword, JsonRequestBehavior.DenyGet);
+                    else
+                    {
+                        //creating token
+                        Session[Sys.userId] = userControl.user_id;
+                        Session[Sys.mail] = userControl.mail;
+                        Session[Sys.firstName] = userControl.first_name;
+                        Session[Sys.lastName] = userControl.last_name;
+                        Session[Sys.birthDate] = userControl.birth_date;
+                        Session[Sys.createDate] = userControl.create_date;
+
+                        Session.Timeout = rememberMe ? 250000 : 720;
+                    }
+                    return Json(LoginStatus.Success, JsonRequestBehavior.DenyGet);
+                }
+                    
+            }
+            catch (Exception)
+            {
+                return Json(LoginStatus.Fail, JsonRequestBehavior.DenyGet);
+            }
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult Register(string mail,string password)
+        {
+            try
+            {
+                using (entities = new wwbEntities())
+                {
+                    ////mail control
+                    Users userControl = entities.Users
+                            .Where(u => u.mail == mail)
+                            .FirstOrDefault();
+
+                    if (userControl != null)
+                        return Json(RegisterStatus.MailExist, JsonRequestBehavior.AllowGet);
+
+                    string hashPassword = Crypto.HashPassword(password);
+
+
+                    Users user = new Users();
+                    user.password = hashPassword;
+                    user.mail = mail;
+                    user.create_date = DateTime.Now;
+
+                    entities.Users.Add(user);
+
+
+                    entities.SaveChanges();
+                }
+            }
+            catch (Exception)
+            {
+                return Json(RegisterStatus.Fail, JsonRequestBehavior.AllowGet);
+            }
+
+
+            return Json(RegisterStatus.Success, JsonRequestBehavior.AllowGet);
+        }
+    }
+
+}
---
+++ WorldWordBox/Models/Sys.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WorldWordBox.Models
+{
+    public abstract class Sys
+    {
+        public static string userId = "user_id";
+        public static string mail = "mail";
+        public static string firstName = "first_name";
+        public static string lastName = "last_name";
+        public static string birthDate = "birth_date";
+        public static string createDate = "create_date";
+
+
+        public static string yandexTranslateApiKey = "trnsl.1.1.20170625T120448Z.da67eaaf57c1c552.5d7f2ad51c9331741b824c8d21e051c8b7090255";
+
+
+    }
+}
